2292d2d5c1c8583fb89de595ad58d7f32efd4a82
Maxwell Jung <jungmaxwell@gmail.com>
2026-05-10 22:16:13 -0700

    clean up

--- modules/counter/sim/counter_tb.sv
+++ modules/counter/sim/counter_tb.sv
@@ -5,17 +5,17 @@ module counter_tb ();
     localparam int ClkPeriod = 10; // 100 MHz clock
 
     logic clk;
-    logic reset;
-    logic enable;
+    logic rst;
+    logic en;
     wire [NumBits-1:0] count;
 
     counter #(
-        .NumBits(NumBits)
+        .CountBits(NumBits)
     ) counter_inst (
-        .clk(clk),
-        .reset(reset),
-        .enable(enable),
-        .count(count)
+        .clk_in (clk),
+        .rst_in (rst),
+        .en_in  (en),
+        .count_out (count)
     );
 
     initial clk = 0;
@@ -29,33 +29,33 @@ module counter_tb ();
 
     initial begin
         // pause counter
-        enable = 0;
+        en = 0;
 
-        // hold reset for 10 clock cycles
-        reset = 1;
+        // hold rst for 10 clock cycles
+        rst = 1;
         repeat (10) @(posedge clk);
-        reset = 0;
+        rst = 0;
 
 
         // start counter for 50 ns
         repeat (10) @(posedge clk);
-        enable = 1;
+        en = 1;
         repeat (5) @(posedge clk);
-        enable = 0;
+        en = 0;
 
         assert (count != 0)
-            else $error("Counter should not be zero after reset is deasserted");
+            else $error("Counter should not be zero after rst is deasserted");
 
         // resume counter for 50 ns
         repeat (10) @(posedge clk);
-        enable = 1;
+        en = 1;
         repeat (5) @(posedge clk);
-        enable = 0;
+        en = 0;
 
-        // hold reset for 50 ns
-        reset = 1;
+        // hold rst for 50 ns
+        rst = 1;
         repeat (5) @(posedge clk);
-        reset = 0;
+        rst = 0;
 
         repeat (20) @(posedge clk);
         $finish;
--- modules/counter/src/counter.sv
+++ modules/counter/src/counter.sv
@@ -1,21 +1,22 @@
 `timescale 1ns / 1ps
-
 `default_nettype none
-module counter #(
-    parameter int NumBits = 8
-) (
-    input logic clk,
-    input logic reset,
-    input logic enable,
 
-    output logic [NumBits-1:0] count
+module counter #(
+    parameter int CountBits = 8
+) (
+    input wire clk_in,
+    input wire rst_in,
+
+    input wire en_in,
+
+    output logic [CountBits-1:0] count_out
 );
 
-    always_ff @(posedge clk) begin
-        if (reset) begin
-            count <= 0;
-        end else if (enable) begin
-            count <= count + 1;
+    always_ff @(posedge clk_in) begin
+        if (rst_in) begin
+            count_out <= 0;
+        end else if (en_in) begin
+            count_out <= count_out + 1;
         end
     end
 
